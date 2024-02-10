259687c77404ee82639ebcee0e8491a54613777e
KARTHIKEYAN R <karthikeyanr2703@gmail.com>
2024-02-10 10:48:12 +0530

    files added comming soon

---
+++ script.js
@@ -0,0 +1,25 @@
+let countDownDate = new Date("Feb 28, 2024 00:00:00").getTime();
+let buttons = document.querySelectorAll("#button")
+
+
+let x = setInterval(function () {
+  let now = new Date().getTime();
+  console.log(now, countDownDate);
+  let distance = countDownDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").textContent = days;
+  document.getElementById("hours").textContent = hours;
+  document.getElementById("minutes").textContent = minutes;
+  document.getElementById("seconds").textContent = seconds;
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("days").textContent = "00";
+    document.getElementById("hours").textContent = "00";
+    document.getElementById("minutes").textContent = "00";
+    document.getElementById("seconds").textContent = "00";
+  }
+}, 1000);
